03f909b4cc8b25c25329c3537704ccbafb43d7de
k-ISB <egoukister1004@outlook.com>
2019-06-23 08:20:53 +0900

    [Finish]3-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+#refileのエラー改善
+Refile.secret_key = '4afeefc003630eea80c4eac1108bfb10d8060c84fbbcaea69576cc3212f0f284a6574c5ea63eac3a3a874d52d13663b876b16aac841ab94b453a518212555441'
